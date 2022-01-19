7df57835ed88657e8337c1fb4634c8eab95873cb
myoung37 <79553827+myoung37@users.noreply.github.com>
2022-01-19 14:48:55 -0600

    Initialized Airport, Airline and Flight

--- OOP HW 1/src/TravelManager.java
+++ OOP HW 1/src/TravelManager.java
@@ -2,6 +2,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class TravelManager {
     public static void main(String[] args) {
+        Airport airport = new Airport("ORD");
+        Airline airline = new Airline("United");
         Flight testFlight = new Flight("United", "ORD","MCO","2319","0600");
     }
 }
