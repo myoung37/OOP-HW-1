8be293ba8581c81e3cb63075cf7a67a2e1360b4f
myoung37 <79553827+myoung37@users.noreply.github.com>
2022-02-08 21:25:10 -0600

    02/08/22

--- OOP HW 1/src/CommercialFlight.java
+++ OOP HW 1/src/CommercialFlight.java
@@ -2,43 +2,38 @@ import java.util.Date;
 import java.util.Objects;
 import java.util.UUID;
 
+import Exception.NullParameterException;
+
 public class CommercialFlight {
     private Airline airline;
-    private Airport origine;
+    private Airport origin;
     private Airport destination;
     private UUID flightNumber;
     private Date departureTime;
 
-    public CommercialFlight(Airline airline, Airport origin, Airport destination, String flightNum, Date departureTime) {
+    public CommercialFlight(Airline airline, Airport origin, Airport destination) throws NullParameterException {
     setAirline(airline);
-    setOrigine(origine);
+    setOrigin(origin);
     setDestination(destination);
     setFlightNumber();
     setDepartureTime();
     }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommercialFlight flight = (CommercialFlight) o;
-        return airline.equals(flight.airline) && origine.equals(flight.origine) && destination.equals(flight.destination) && Objects.equals(flightNumber, flight.flightNumber) && Objects.equals(departureTime, flight.departureTime);
-    }
 
     @Override
     public int hashCode() {
-        return Objects.hash(airline, origine, destination, flightNumber, departureTime);
+        return Objects.hash(airline, origin, destination, flightNumber, departureTime);
     }
 
     @Override
     public String toString() {
         return "Flight{" +
-                "airline=" + airline +
-                ", origine=" + origine +
-                ", destination=" + destination +
-                ", flightNumber='" + flightNumber + '\'' +
-                ", departureTime=" + departureTime +
+                "airline=" + getAirline() +
+                ", origin=" + getOrigin() +
+                ", destination=" + getDestination() +
+                ", flightNumber='" + getFlightNumber() + '\'' +
+                ", departureTime=" + getDepartureTime() +
                 '}';
     }
 
@@ -74,11 +69,22 @@ public class CommercialFlight {
         this.departureTime = new Date(2022, 04, 22, 22, 30);
     }
 
-    public Airport getOrigine() {
-        return origine;
+    public Airport getOrigin() {
+        return origin;
     }
 
-    public void setOrigine(Airport origine) {
-        this.origine = origine;
+    private void setOrigin(Airport origin) throws NullParameterException {
+        if(origin.equals(null)){
+            throw new NullParameterException("origin cannot be null");
+        }
+        this.origin = origin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommercialFlight that = (CommercialFlight) o;
+        return Objects.equals(airline, that.airline) && Objects.equals(origin, that.origin) && Objects.equals(destination, that.destination) && Objects.equals(flightNumber, that.flightNumber) && Objects.equals(departureTime, that.departureTime);
     }
 }
